937db6e88a6a05f510c7eaef0ec690e4b6b6a818
忆扬 <liukuang.lk@alibaba-inc.com>
2020-06-16 16:03:02 +0800

    Update transformer

--- transformer.py
+++ transformer.py
@@ -12,7 +12,7 @@ class PositionalEncoding(nn.Module):
     def __init__(self, d_model, dropout=0.1, max_len=8):
         super(PositionalEncoding, self).__init__()
         self.dropout = nn.Dropout(p=dropout)
-        pe = torch.zeros(max_len, d_model)
+        pe = torch.zeros(max_len, d_model)  # [T,D]
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
         div_term = torch.exp(torch.arange(
             0, d_model, 2).float() * (-math.log(10000.0) / d_model))
@@ -22,7 +22,19 @@ class PositionalEncoding(nn.Module):
         self.register_buffer('pe', pe)
 
     def forward(self, x):
-        x = x + self.pe[:x.size(0), :]
+        x = x + self.pe[:x.size(0), :]  # [N,T,D]
+        return self.dropout(x)
+
+
+class LearnedPositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len=8, dropout=0.0):
+        super(LearnedPositionalEncoding, self).__init__()
+        pe = torch.randn(max_len, d_model)  # [T,D]
+        self.pe = nn.Parameter(pe)
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, x):
+        x = x + self.pe  # [N,T,D]
         return self.dropout(x)
 
 
@@ -36,6 +48,7 @@ class TransformerModel(nn.Module):
 
         self.encoder = nn.Linear(in_channels, mid_channels)
         self.emb = PositionalEncoding(mid_channels)
+        self.norm = nn.LayerNorm(mid_channels)
         encoder_layer = nn.TransformerEncoderLayer(
             d_model=mid_channels, nhead=8)
         self.transformer = nn.TransformerEncoder(
@@ -43,10 +56,13 @@ class TransformerModel(nn.Module):
         self.decoder = nn.Linear(mid_channels, out_channels)
 
     def forward(self, x):
-        out = self.encoder(x)
+        out = self.encoder(x)  # [N,T,D]
         out = self.emb(out)
+        out = self.norm(out)
+        out = out.permute(1, 0, 2)  # [N,T,D] -> [T,N,D]
         out = self.transformer(out)
         out = self.decoder(out)
+        out = out.permute(1, 0, 2)  # [T,N,D] -> [N,T,D]
         return out
 
 
@@ -62,7 +78,7 @@ def test():
     batch = 1
     seq = 3
     in_channels = 4
-    x = torch.randn(seq, batch, in_channels)
+    x = torch.randn(batch, seq, in_channels)
     y = model(x)
     print(y.shape)
 
